959f23b7335ccf3c63a0a4b4f714f8c1440c8de2
Mohammad <mohammadmahone@gmail.com>
2022-02-05 18:05:30 +0330

    Create pages and add firebase for sign up and login.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-function App() {
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './Components/Navbar'
+import Explore from './Pages/Explore'
+import Offers from './Pages/Offers'
+import Profile from './Pages/Profile'
+import SignIn from './Pages/SignIn'
+import SignUp from './Pages/SignUp'
+import ForgotPassword from './Pages/ForgotPassword'
+
+const App = () =>
+{
   return (
     <>
-      <h1>My project</h1>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Explore />} />
+          <Route path='/offers' element={<Offers />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+        </Routes>
+        <Navbar />
+      </Router>
     </>
   )
 }
---
+++ src/Pages/Profile.jsx
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react'
+import { getAuth } from 'firebase/auth'
+
+const Profile = () => 
+{
+    const [user, setUser] = useState(null)
+
+    const auth = getAuth()
+
+    useEffect(() =>
+    {
+        setUser(auth.currentUser)
+    }, [])
+
+    return user ? <h1>Welcome Back {user.displayName}</h1> : 'Not Logged In'
+}
+ 
+export default Profile
