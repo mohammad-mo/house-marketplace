ca6e327e3d0222a0b3b2c7019194e8ef7c88eb5a
Mohammad <mohammadmahone@gmail.com>
2022-02-07 09:46:55 +0330

    Edit category component.

--- src/Pages/Category.jsx
+++ src/Pages/Category.jsx
@@ -1,7 +1,71 @@
+import React, { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import { collection, getDocs, query, where, orderBy, limit, startAfter } from "firebase/firestore"
+import { db } from "../firebase.config"
+import { toast } from "react-toastify"
+import Spinner from "../Components/Spninner"
+
 const Category = () => 
 {
+    const [listings, setListings] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    const params = useParams()
+
+    useEffect( () =>
+    {
+        const fetchListings = async () =>
+        {
+            try
+            {
+                // Get reference
+                const listingsRef = collection(db, 'listings')
+
+                // Create a query
+                const q = query(listingsRef, where('type', '==', params.categoryName), orderBy('timestamp', 'desc', limit(10)))
+
+                // Execute query
+                const querySnap = await getDocs(q)
+
+                const listings = []
+
+                querySnap.forEach((doc) => 
+                {
+                    return listings.push({
+                        id: doc.id,
+                        data: doc.data()
+                    })
+                })
+
+                setListings(listings)
+                setLoading(false)
+            }
+            catch(err)
+            {
+                toast.error('Could not fetch listings')
+            }
+        }
+        fetchListings()
+    }, [params.categoryName])
+
     return (
-        <div>Category</div>
+        <div className="category">
+            <header>
+                <p className="pageHeader">
+                    {params.categoryName === 'rent' ? 'Places for rent' : 'Places for sale'}
+                </p>
+            </header>
+            {loading ? <Spinner /> : listings && listings.length > 0 ?
+            <>
+                <main>
+                    <ul className="categoryListings">
+                        {React.Children.toArray(listings.map((listing) => (
+                            <h3>{listing.data.name}</h3>
+                        )))}
+                    </ul>
+                </main>
+            </> : <p>No listing for {params.categoryName}</p>}
+        </div>
     )
 }
  
